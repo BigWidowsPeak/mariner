90614a7819e6e9253f6d4f61115eb8a57684394d
Sean Spencer <sean_spencer@me.com>
2018-03-27 02:25:12 -0700

    Installed fontawesome's react module, and converted serviceSelector compoent's icons to native FontAwesome components. I can now apply fills to the icons, however they don't respond to width properties in CSS. Investigating any potential style conflicts.

--- client/src/components/ServiceSelector/ServiceSelector.jsx
+++ client/src/components/ServiceSelector/ServiceSelector.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './ServiceSelector.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 import YouTubeLogo from '../../../assets/youtube-square.svg';
 import GoogleLogo from '../../../assets/google.svg';
@@ -11,12 +12,18 @@ const serviceSelector = (props) => (
     <p className={classes.loginChoicePrompt}>
       Choose a service to start with.
     </p>
+    {/* {/* <a href="http://localhost:3000/auth/youtube">} */}
+    {/* <img src={YouTubeLogo} className={classes.loginYoutube}></img> */}
+    {/* </a>*/}
+    {/* <img src={GoogleLogo} className={classes.loginGoogle}></img> */}
+    {/* <img src={TwitterLogo} className={classes.loginTwitter}></img> */}
+    {/* <img src={TwitchLogo} className={classes.loginTwitch}></img> */}
     <a href="http://localhost:3000/auth/youtube">
-    <img src={YouTubeLogo} className={classes.loginYoutube}></img>
+    <FontAwesomeIcon icon={['fab', 'youtube-square']} listItem size="2x" className={classes.loginYoutube} />
     </a>
-    <img src={GoogleLogo} className={classes.loginGoogle}></img>
-    <img src={TwitterLogo} className={classes.loginTwitter}></img>
-    <img src={TwitchLogo} className={classes.loginTwitch}></img>
+    <FontAwesomeIcon icon={['fab', 'google']} listItem size="2x" className={classes.loginGoogle} /> 
+    <FontAwesomeIcon icon={['fab', 'twitter']} listItem size="2x" className={classes.loginTwitter} /> 
+    <FontAwesomeIcon icon={['fab', 'twitch']} listItem size="2x" className={classes.loginTwitch} /> 
   </div>
 );
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import solid from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(brands, solid);
+
 ReactDOM.render(<App />, document.getElementById('root'));
